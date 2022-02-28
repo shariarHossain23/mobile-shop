71975ec5561dd0997975b635f20b5cd0636b8232
shariarhossain23e <shariarhossain23@gamil.comm>
2022-02-28 23:29:53 +0600

    call details api

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const loadData = async () => {
 
     //  clear display\]
     searchInput.value = ""
+    document.getElementById("phone-show").textContent = ""
 
     // fetching data
    const url = `https://openapi.programming-hero.com/api/phones?search=${searchValue}`;
@@ -20,21 +21,44 @@ const loadData = async () => {
 // show ui load data
 const showDisplayData = mobiles =>{
     const showPhone = document.getElementById("phone-show");
-    mobiles.forEach(mobile => {
-        const newDiv = document.createElement("div");
-        newDiv.classList.add("col-md-4");
-        newDiv.innerHTML = `
-        <div class="card mt-4">
-                <div>
-                    <img src="${mobile.image}" alt="" />
-                </div>
-                <h5>Name: ${mobile.phone_name}</h5>
-                <h5>Model:${mobile.brand}</h5>
-                <div>
-                  <button onclick="" class="mobile-btn">Explore</button>
-                </div>
-        </div>
-        `
-        showPhone.appendChild(newDiv);
-    });
+
+    if(mobiles.length === 0){
+      document.getElementById("error-text").innerText = "No result found"
+    }
+    else if(mobiles.length === 20){
+        
+    }
+    else{
+        mobiles.forEach(mobile => {
+        
+            const newDiv = document.createElement("div");
+            newDiv.classList.add("col-md-4");
+            newDiv.innerHTML = `
+            <div class="card mt-4">
+                    <div>
+                        <img src="${mobile.image}" alt="" />
+                    </div>
+                    <h5>Name: ${mobile.phone_name}</h5>
+                    <h5>Model:${mobile.brand}</h5>
+                    <div>
+                    <button onclick="loadDetails('${mobile.slug}')" class="mobile-btn">
+                    Explore
+                  </button>
+                    </div>
+            </div>
+            `
+            // console.log(mobile.slug)
+            showPhone.appendChild(newDiv);
+            // clear display
+            document.getElementById("error-text").innerText = "";
+        });
+    }
+   
+}
+
+//  calling details
+const loadDetails = details =>{
+    fetch(`https://openapi.programming-hero.com/api/phone/${details}`)
+    .then(res => res.json())
+    .then(data => console.log(data.data))
 }
